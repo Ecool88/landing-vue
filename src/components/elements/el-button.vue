6de5fa481764773589786a4f9237d4d6f70e67c7
Ecool88 <kulik0v.e.e@yandex.ru>
2021-06-22 09:07:22 +0300

    assembly for page layout

---
+++ src/components/elements/el-button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="el-button">
+      <span>{{title}}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "el-button",
+  data() {
+    return {}
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'button'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .el-button{
+    background-color: $bg-color-blue;
+    border-color: $bg-color-blue;
+    color: $text-white;
+    display: flex;
+    height: 100%;
+    width: 100%;
+    font-weight: inherit;
+    font-size: inherit;
+  }
+  span{
+    margin: auto;
+    padding: 15px 35px;
+    font-weight: inherit;
+    font-size: inherit;
+  }
+</style>
